24f2f5363c6b64ed64290d1bfe0b6451cf528de8
Sofiaa05 <Sofiabbasi05@gmail.com>
2025-02-19 08:48:00 +0530

    Created about page - sofia

--- frontend-pawsitive/src/App.js
+++ frontend-pawsitive/src/App.js
@@ -2,9 +2,11 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
 
+// Importing Pages
 import Homepage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import SignupPage from './pages/SignupPage';
+import AboutUs from './pages/AboutUs';
 
 function App() {
   return (
@@ -13,6 +15,7 @@ function App() {
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
+        <Route path="/about" element={<AboutUs />} />
       </Routes>
     </Router>
   );
---
+++ frontend-pawsitive/src/pages/AboutUs.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function AboutUs(){
+    return (
+        <h1>This is about us.</h1>
+    );
+}
+
+export default AboutUs;
