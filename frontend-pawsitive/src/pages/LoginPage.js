f7f3889784edc462b5c4a5e1ac7e1466da6587b0
Sofiaa05 <Sofiabbasi05@gmail.com>
2025-02-19 14:38:35 +0530

    login

--- frontend-pawsitive/src/pages/LoginPage.js
+++ frontend-pawsitive/src/pages/LoginPage.js
@@ -3,7 +3,17 @@ import React from 'react';
 const LoginPage = () => {
   return (
     <div>
-      <h1>This is login page</h1>
+      <div className='login-form'>
+        <form>
+          <label for='email'>Email:</label>
+          <input type='email' id='email' ></input>
+          <br></br>
+          <label for='password'>Password:</label>
+          <input type='password' id='password'></input>
+          <br></br>
+          <input type='submit' value="Submit"></input>
+        </form>
+      </div>
     </div>
   );
 };
