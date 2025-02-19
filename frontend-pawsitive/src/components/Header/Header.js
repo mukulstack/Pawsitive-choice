6695aa8335f99f271109bc9e24c16290ccf23435
mukulstack <mukuldalal544@gmail.com>
2025-02-19 23:09:32 +0530

    Homepage Updated.

--- frontend-pawsitive/src/components/Header/Header.js
+++ frontend-pawsitive/src/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './Header.css';  
 
-function Header() {
+function Header({children}) {
     return (
         <header className="header">
             <div className="logo">
                 <h1>Pawsitive Choice</h1>  
+            </div>
+            <div className="buttons-container">
+                {children}
             </div>
         </header>
     );
--- frontend-pawsitive/src/pages/HomePage/HomePage.js
+++ frontend-pawsitive/src/pages/HomePage/HomePage.js
@@ -1,10 +1,26 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
+import { useNavigate } from 'react-router-dom';
 
 const HomePage = () => {
+  const navigate = useNavigate();
+
+  const handleLoginClick = () => {
+    navigate('/login');
+  };
+
+  const handleSignUpClick = () => {
+    navigate('/signup');
+  };
+
   return (
     <div>
-      <Header />
+      <Header>
+        <div className="buttons">
+          <button onClick={handleLoginClick}>Log in</button>
+          <button onClick={handleSignUpClick}>Sign up</button>
+        </div>
+      </Header>
     </div>
   );
 };
